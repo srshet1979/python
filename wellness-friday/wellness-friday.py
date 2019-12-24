4163848cc84685c8769a15bde59467b2169989d6
srshet1979 <srshet@gmail.com>
2019-12-24 20:58:22 +0530

    wellness friday for an year

---
+++ wellness-friday/wellness-friday.py
@@ -0,0 +1,12 @@
+import calendar
+print("Wellness fridays will be:")
+year=2020
+for m in range(1,13):
+    cal = calendar.monthcalendar(year,m)
+    week2=cal[1]
+    week3=cal[2]
+    if week1[calendar.FRIDAY] != 0:
+        wf = week2[calendar.FRIDAY]
+    else:
+        wf = week3[calendar.FRIDAY]
+    print ("%10s %2d %4s" % (calendar.month_name[m], wf, year))
